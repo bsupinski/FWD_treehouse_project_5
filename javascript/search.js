af92ed410f9f9e61c2c94c87efc42a3ff5e69869
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2021-02-07 12:46:21 -0500

    Add files via upload

--- javascript/search.js
+++ javascript/search.js
@@ -1,16 +1,16 @@
 function img_search() {
     let input = document.getElementById('searchbar').value
     input = input.toLowerCase();
-    let x = document.getElementsByClassName('scenary');
+    let imgClass = document.getElementsByClassName('scenary');
 
-    for ( i = 0; i < x.length; i++) {
-        if( !x[i].getAttribute('data-caption').toLowerCase().includes(input)) {
-            x[i].style.display='none';
-            x[i].style.listStyleType='none';
+    for ( i = 0; i < imgClass.length; i++) {
+        if( !imgClass[i].getAttribute('data-caption').toLowerCase().includes(input)) {
+            imgClass[i].style.display='none';
+            imgClass[i].style.listStyleType='none';
         }
         else {
-            x[i].style.display='list-item'
-            x[i].style.listStyleType='none';
+            imgClass[i].style.display='list-item'
+            imgClass[i].style.listStyleType='none';
         }
     }
 
